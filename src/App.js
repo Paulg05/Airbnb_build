606d337f30aeed1c2142f492450b7a164a446024
Paul Gonzalez <robertgonzalez018@gmail.com>
2020-12-10 12:23:26 -0800

    cleaned up app structure and deleted testing files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+
+    //BEM
+    <div className="App">
+      <h1>airbnb build</h1>
+        {/* header */}
+        {/* banner */}
+        {/* cards */}
+        {/* footer */}
+
+
+    </div>
+  );
+}
+
+export default App;
